a8201d04f00d25b21474bfa89b22abea19493102
Saad Khan <khan.ssjc@gmail.com>
2023-09-09 10:53:04 -0400

    react/ refactor for pages and routing
    * install react router deps
    * abstract useReducer into it's own component
    * refactor App.tsx to display each component on it's own page

--- react/02-intro-to-hooks/src/App.tsx
+++ react/02-intro-to-hooks/src/App.tsx
@@ -1,55 +1,14 @@
-import React, { useReducer } from 'react';
-import Greeting from "./Greeting";
-
-const reducer = (state: any, action: any) => {
-
-  switch(action.type) {
-    case "enteredName":
-      
-      if(state.enteredName === action.payload) {
-        return state;
-      }
-
-      return {
-         ...state,
-         enteredName: action.payload 
-      };
-    case "message":
-      return {
-        ...state,
-        message: `Hello, ${action.payload}`
-      };
-    default: 
-        throw new Error("Invalid action type " + action.type);
-  }
-}
-
-const initialState = {
-  enteredName: "",
-  message: ""
-}
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import UsingReducer from "./pages/using-reducer";
 
 function App() {
-  const [{message, enteredName}, dispatch] = useReducer(reducer, initialState);
-  
-  const onChangeName = (e:React.ChangeEvent<HTMLInputElement>) => {
-
-    dispatch({
-      type: "enteredName",
-      payload: e.target.value
-    });
-
-    dispatch({ 
-      type: "message",
-      payload: e.target.value
-    })
-  }
-
   return (
     <>
-      <input value={enteredName} 
-        onChange={onChangeName} />
-      <Greeting message={message} />
+      <Routes>
+        <Route path="using-reducer" element={<UsingReducer />}>
+        </Route>
+      </Routes>
     </>
   )
 }
